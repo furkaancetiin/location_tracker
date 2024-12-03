487098865244e036dc892a569bb3a784e5fd99ba
Furkan <furkan.cetin.020@gmail.com>
2024-12-03 22:49:16 +0300

    permission part refactored

--- lib/service/location_service.dart
+++ lib/service/location_service.dart
@@ -1,55 +1,59 @@
+import 'dart:io';
+
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class LocationService {
-  Future<void> checkAndRequestPermissions() async {
+  Future<bool> checkAndRequestPermissions() async {
     try {
-          bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-          if (!serviceEnabled) {
-            await Geolocator.openLocationSettings();
-            return;
-          }
-
-          if (await Permission.notification.isDenied) {
-            PermissionStatus notificationPermission = await Permission.notification.request();
-            if(notificationPermission == PermissionStatus.denied){
-              print('Notification izni reddedildi.');
-              return;
-            }
-          }
-
-          LocationPermission permission = await Geolocator.checkPermission();
-          if (permission == LocationPermission.denied) {
-            permission = await Geolocator.requestPermission();
-            if (permission == LocationPermission.denied) {
-              print('Konum izni reddedildi.');
-              return;
-            }
-          }
-
-          if (permission == LocationPermission.deniedForever) {
-            print('Konum izni kalıcı olarak reddedildi.');
-            return;
-          }
-
-          if(permission != LocationPermission.always){
-            print("Always izni gerekli. İzin isteme...");
-            permission = await Geolocator.requestPermission();
-          }
-
-          if (permission == LocationPermission.always) {
-            print("Always izni başarıyla alındı.");
-          } else {
-            print("Always izni alınamadı.");
-          }
-        } catch (e) {
-          print('Konum alma hatası: $e');
-          return null;
+      bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!isLocationServiceEnabled) {
+        isLocationServiceEnabled = await Geolocator.openLocationSettings();
+        if(!isLocationServiceEnabled){
+          return false;
         }
+      }
+
+      PermissionStatus whileInUse = await Permission.locationWhenInUse.status;
+      if(!whileInUse.isGranted){
+        whileInUse = await Permission.locationWhenInUse.request();
+        if(!whileInUse.isGranted){
+          print('whileInUse konum izni reddedildi.');
+          return false;
+        }else if(whileInUse.isGranted){
+          PermissionStatus isLocationAlways = await Permission.locationAlways.status;
+          if (!isLocationAlways.isGranted) {
+            isLocationAlways = await Permission.locationAlways.request();
+            if (!isLocationAlways.isGranted) {
+              print('Always konum izni reddedildi.');
+              return false;
+            }
+          }
+        }
+      }
+
+      if (await Permission.notification.isDenied) {
+        PermissionStatus notificationPermission = await Permission.notification.request();
+        if (notificationPermission == PermissionStatus.denied) {
+          print('Notification izni reddedildi.');
+          return false;
+        }
+      }
+
+      if (Platform.isAndroid) {
+        if (!await FlutterForegroundTask.isIgnoringBatteryOptimizations) {
+          await FlutterForegroundTask.requestIgnoreBatteryOptimization();
+        }
+      }
+      return true;
+    } catch (e) {
+      print('İzin alma hatası: $e');
+      return false;
+    }
   }
 
-
-  Future<Map<String, dynamic>?> getCurrentLocation() async{
+  Future<Map<String, dynamic>?> getCurrentLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
